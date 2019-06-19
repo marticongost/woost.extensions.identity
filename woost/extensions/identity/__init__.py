6e2c72c571cb668adb75feadec92b010e1ac10aa
Marti Congost <marti.congost@whads.com>
2019-06-19 17:43:15 +0200

    Added a missing import statement for the extension's database migration module

--- woost/extensions/identity/__init__.py
+++ woost/extensions/identity/__init__.py
@@ -2,6 +2,6 @@
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
-from . import settings, admin, overlays, facebook, google
+from . import settings, admin, overlays, facebook, google, migration
 from .identityprovider import IdentityProvider
 
